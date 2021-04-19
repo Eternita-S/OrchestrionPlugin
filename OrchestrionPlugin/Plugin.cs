4bbd2d43a7f6ad4978013f9a936d6381ae03da3b
Eternita <lysithea.star@gmail.com>
2021-04-19 04:25:31 +0300

    oops

--- OrchestrionPlugin/Plugin.cs
+++ OrchestrionPlugin/Plugin.cs
@@ -40,7 +40,7 @@ namespace OrchestrionPlugin
             this.configuration.Initialize(pluginInterface);
             this.enableFallbackPlayer = this.configuration.UseOldPlayback;
             
-            this.localDir = Path.GetDirectoryName("Y:\\Users\\LSS\\source\\repos\\OrchestrionPlugin\\OrchestrionPlugin\\bin\\Debug\\"/*Assembly.GetExecutingAssembly().Location*/);
+            this.localDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
             var songlistPath = Path.Combine(this.localDir, songListFile);
             this.songList = new SongList(songlistPath, this.configuration, this, this);
